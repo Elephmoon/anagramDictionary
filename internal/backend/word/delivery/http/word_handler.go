759211067a55e28b78cbf5cc6703c19ca0ba39ba
Elephmoon <alexandr.kizilow@gmail.com>
2020-11-20 08:35:24 +0300

    implement api methods for work with words

---
+++ internal/backend/word/delivery/http/word_handler.go
@@ -0,0 +1,88 @@
+package http
+
+import (
+	"encoding/json"
+	"github.com/Elephmoon/anagramDictionary/internal/backend/word"
+	"github.com/Elephmoon/anagramDictionary/internal/helpers"
+	"github.com/Elephmoon/anagramDictionary/internal/models"
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+type WordHandler struct {
+	WordUsecase word.Usecase
+}
+
+func NewWordUsecase(router *mux.Router, wordUsecase word.Usecase) {
+	handler := WordHandler{
+		WordUsecase: wordUsecase,
+	}
+	wordSubRouter := router.PathPrefix("/words").Subrouter()
+	wordSubRouter.HandleFunc("", handler.get).Methods("GET")
+	wordSubRouter.HandleFunc("/{word}", handler.delete).Methods("DELETE")
+	wordSubRouter.HandleFunc("", handler.addWords).Methods("POST")
+}
+
+func (wh *WordHandler) get(w http.ResponseWriter, r *http.Request) {
+	queryParams := r.URL.Query()
+	offset := queryParams.Get("offset")
+	limit := queryParams.Get("limit")
+
+	data, err := wh.WordUsecase.ShowDictionary(offset, limit)
+	if err != nil {
+		err := helpers.GenerateHttpErrorResp(w, err)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		return
+	}
+	resp, err := helpers.GenerateHttpResp(true, nil, data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	err = json.NewEncoder(w).Encode(resp)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func (wh *WordHandler) delete(w http.ResponseWriter, r *http.Request) {
+	err := wh.WordUsecase.DeleteWord(mux.Vars(r)["word"])
+	if err != nil {
+		err := helpers.GenerateHttpErrorResp(w, err)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+}
+
+func (wh *WordHandler) addWords(w http.ResponseWriter, r *http.Request) {
+	createReq := &models.CreateReq{}
+	err := json.NewDecoder(r.Body).Decode(createReq)
+	if err != nil {
+		err := helpers.GenerateHttpErrorResp(w, err)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		return
+	}
+	err = wh.WordUsecase.AddWords(createReq.Words)
+	if err != nil {
+		err := helpers.GenerateHttpErrorResp(w, err)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+}
--- internal/helpers/helpers.go
+++ internal/helpers/helpers.go
@@ -1,7 +1,9 @@
 package helpers
 
 import (
+	"encoding/json"
 	"github.com/pkg/errors"
+	"net/http"
 	"sort"
 	"strconv"
 	"strings"
@@ -18,6 +20,12 @@ var (
 type QueryParams struct {
 	Offset int
 	Limit  int
+}
+
+type HttpResp struct {
+	Success bool        `json:"success"`
+	Error   string      `json:"error,omitempty"`
+	Data    interface{} `json:"data,omitempty"`
 }
 
 func GetQueryParams(offset, limit string) (*QueryParams, error) {
@@ -49,4 +57,29 @@ func SortWord(word string) string {
 	splitWord := strings.Split(word, "")
 	sort.Strings(splitWord)
 	return strings.Join(splitWord, "")
+}
+
+func GenerateHttpErrorResp(w http.ResponseWriter, err error) error {
+	resp, err := GenerateHttpResp(false, err, nil)
+	if err != nil {
+		return err
+	}
+	err = json.NewEncoder(w).Encode(resp)
+	if err != nil {
+		return err
+	}
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(http.StatusBadRequest)
+	return nil
+}
+
+func GenerateHttpResp(success bool, err error, data interface{}) ([]byte, error) {
+	httpResp := HttpResp{
+		Success: success,
+		Data:    data,
+	}
+	if err != nil {
+		httpResp.Error = err.Error()
+	}
+	return json.Marshal(httpResp)
 }
