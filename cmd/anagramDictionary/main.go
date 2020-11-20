77b429946e049c750d90f53ce058e6f5bfc9d9a9
Elephmoon <alexandr.kizilow@gmail.com>
2020-11-20 17:51:39 +0300

    add swagger specification

--- cmd/anagramDictionary/main.go
+++ cmd/anagramDictionary/main.go
@@ -9,6 +9,7 @@ import (
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"github.com/pkg/errors"
+	"html/template"
 	"net/http"
 )
 
@@ -19,13 +20,13 @@ func main() {
 		panic(err)
 	}
 	router := mux.NewRouter()
+	swag := http.StripPrefix("/swagger-ui/", http.FileServer(http.Dir("./internal/static/swagger_dist")))
+	router.PathPrefix("/swagger-ui/").Handler(swag)
+	router.HandleFunc("/api.yml", initSwaggerSpec(conf.APIConfig))
 	apiRouter := router.PathPrefix("/api/v1").Subrouter()
 	routes.InitApiRoutes(apiRouter, dbConn)
-	server := &http.Server{
-		Addr:    fmt.Sprintf(":%s", conf.APIConfig.Port),
-		Handler: apiRouter,
-	}
-	panic(server.ListenAndServe())
+	addr := fmt.Sprintf(":%s", conf.APIConfig.Port)
+	panic(http.ListenAndServe(addr, router))
 }
 
 func initDB(conf config.DBConfig) (*gorm.DB, error) {
@@ -37,4 +38,22 @@ func initDB(conf config.DBConfig) (*gorm.DB, error) {
 	}
 	migrations.All(dbConn)
 	return dbConn, nil
+}
+
+func initSwaggerSpec(conf config.APIConfig) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		data := struct {
+			Host string
+			Port string
+		}{
+			Host: "localhost",
+			Port: conf.Port,
+		}
+		tmpl := template.Must(template.ParseFiles("./api/api.yml"))
+		err := tmpl.Execute(w, data)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
 }
